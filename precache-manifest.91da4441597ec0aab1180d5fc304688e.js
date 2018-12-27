a2df414080d38169504884398781673dc79231dd
XmadogX <r666555444@gmail.com>
2018-12-27 21:22:04 +0300

    first_commit

---
+++ precache-manifest.91da4441597ec0aab1180d5fc304688e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e40aa70fdba3a166788",
+    "url": "/static/js/main.6e40aa70.chunk.js"
+  },
+  {
+    "revision": "78c19e8d0838c6866779",
+    "url": "/static/js/1.78c19e8d.chunk.js"
+  },
+  {
+    "revision": "6e40aa70fdba3a166788",
+    "url": "/static/css/main.eccaae84.chunk.css"
+  },
+  {
+    "revision": "a7be5fb26e504b886bc1c3dd94927c24",
+    "url": "/index.html"
+  }
+];
